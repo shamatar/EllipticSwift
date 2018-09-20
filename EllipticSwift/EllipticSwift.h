c0679b0f2300f12851d7a0cae19ed2dda1adeb77
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-09-20 22:58:52 +0300

    Initial Commit

---
+++ EllipticSwift/EllipticSwift.h
@@ -0,0 +1,19 @@
+//
+//  EllipticSwift.h
+//  EllipticSwift
+//
+//  Created by Alex Vlasov on 20.09.2018.
+//  Copyright © 2018 Alex Vlasov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EllipticSwift.
+FOUNDATION_EXPORT double EllipticSwiftVersionNumber;
+
+//! Project version string for EllipticSwift.
+FOUNDATION_EXPORT const unsigned char EllipticSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EllipticSwift/PublicHeader.h>
+
+
